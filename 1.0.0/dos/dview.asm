2346a9c112684cb1fa09fd7e3aa58b8aa036baf7
missn0body <hulk128t@gmail.com>
2023-10-20 09:57:14 -0700

    Added argument parsing to DOS version

--- 1.0.0/dos/dview.asm
+++ 1.0.0/dos/dview.asm
@@ -1,4 +1,5 @@
 %define NEWLINE	13, 10
+%define ENDL 13, 10, '$'
 
 	bits 16
 	cpu 8086
@@ -16,22 +17,42 @@ _start:
 	cmp	bl, 7EH			; counter overflow; args higher than 127 bytes?
 	ja	_end
 	mov	byte [bx + 81H], '$'	; set end of args to dollar sign
-	lea	dx, [81H]		; load address of args string
-	call	puts			; print it
+	cmp	[81H + 1], byte 45	; is the argv[1][0] a '-'?
+	je	argsParse
 	call	_end
 
 noArgs:
 	lea	dx, [errorPre]
 	call	puts
-	lea	dx, [noArgsMes]
+	lea	dx, [errorMes2]
 	call	puts
 	call	_end
 
-longArgs:
-	lea     dx, [errorPre]
-        call    puts
-	lea	dx, [highArgsMes]
+argsParse:
+	cmp     [81H + 2], byte 104	; is it equal to 'h'?
+        je      useMes			; then show usage
+        cmp     [81H + 2], byte 118	; is it equal to 'v'?
+	je	verMes
+	lea	dx, [errorPre]		; we don't know argument
 	call	puts
+	lea	dx, [errorMes1]
+	call	puts
+	jmp	useMes			; make sure the user knows
+	call	_end			; how to use the program
+
+; displays name, version and usage information
+useMes:
+	lea	dx, [fileName]
+        call    puts
+        lea     dx, [usage]
+        call    puts
+        lea     dx, [flagsEx]
+        call    puts
+        call    _end
+
+verMes:
+	lea     dx, [fileName]
+        call    puts
 	call	_end
 
 _end:
@@ -50,5 +71,13 @@ section .data:
 	mesPre:		db 'view: $'
 	errorPre:	db 'error: $'
 
-	noArgsMes: 	db 'no args given', NEWLINE, '$'
-	highArgsMes: 	db 'argument count higher than 127 bytes, aborting...', NEWLINE, '$'
+	fileName:       db 'Terminal File VIEWer '
+        version:        db '(v. 1.0.0): '
+        signature:      db 'a barebones assembly schtick by anson.', ENDL
+        usage:          db 'Usage: view <file> <-h> <-v>', ENDL
+        flagsEx:        db 09H, '<-h> : display help', NEWLINE, 09H, '<-v> : set verbose mode', ENDL
+
+	errorMes1:      db 'unknown argument', ENDL
+        errorMes2:      db 'not enough arguments', ENDL
+        errorMes3:      db 'file could not be open', ENDL
+        errorMes4:      db 'file could not be read', ENDL
